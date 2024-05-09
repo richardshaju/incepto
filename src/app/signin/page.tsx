a1b10cd7abef35d541f4e8ee55498bbcffec2d7f
Richard Shaju <108459152+richardshaju@users.noreply.github.com>
2024-05-09 19:52:00 +0530

    Update page.tsx

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Input } from "../components/ui/input";
+// import { Input } from "../components/ui/Input";
 // import { auth } from "../../firebase.config";
 // import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 // import { CgSpinner } from "react-icons/cg";
